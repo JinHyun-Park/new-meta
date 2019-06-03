87bfc31bb8b6c99295e17677433f0f34f9a94a3d
jinhyun <jinhyun0504@hanmail.net>
2019-06-04 01:38:47 +0900

    테이블 추가

--- src/components/views/IfRegist.vue
+++ src/components/views/IfRegist.vue
@@ -1,90 +1,92 @@
 <template>
-  <v-container grid-list-md>
-    <v-layout>
-      <v-flex>
-        <div>
-          신청목록
-        </div>
-      </v-flex>
-    </v-layout>
-    <v-divider />
+  <v-app>
+    <v-container grid-list-md>
+      <v-layout>
+        <v-flex>
+          <div>
+            신청목록
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-divider />
 
-    <v-btn-toggle v-model="text">
-      <v-btn
-        text
-        value="left"
-      >
-        EIGW
-      </v-btn>
-      <v-btn
-        text
-        value="center"
-      >
-        EAI
-      </v-btn>
-      <v-btn
-        text
-        value="right"
-      >
-        MCG
-      </v-btn>
-    </v-btn-toggle>
-    <v-layout
-      row
-      wrap
-    >
-      <v-flex
-        d-flex
-        xs9
-        md9
-      >
-        <v-layout
-          column
+      <v-btn-toggle v-model="text">
+        <v-btn
+          text
+          value="left"
         >
-          <v-flex
-            d-flex
-            xs12
-            sm6
-            md6
-          >
-            <v-card>
-              <AddListInRegist />
-              <v-card-text>444</v-card-text>
-              <v-card-text>444</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        d-flex
-        xs3
-        md3
-        color="grey lighten-3"
-        align-self-center
+          EIGW
+        </v-btn>
+        <v-btn
+          text
+          value="center"
+        >
+          EAI
+        </v-btn>
+        <v-btn
+          text
+          value="right"
+        >
+          MCG
+        </v-btn>
+      </v-btn-toggle>
+      <v-layout
+        row
+        wrap
       >
-        <RightSideCard />
-      </v-flex>
-    </v-layout>
+        <v-flex
+          d-flex
+          xs9
+          md9
+        >
+          <v-layout
+            column
+          >
+            <v-flex
+              d-flex
+              xs12
+              sm6
+              md6
+            >
+              <v-card>
+                <RegistList />
+                <AddListInRegist />
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
 
-    <v-divider />
-    <v-layout>
-      <v-flex>
-        <v-sheet>
-          메타 포탈
-        </v-sheet>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        <v-flex
+          d-flex
+          xs3
+          md3
+          color="grey lighten-3"
+          align-self-center
+        >
+          <RightSideCard />
+        </v-flex>
+      </v-layout>
+
+      <v-divider />
+      <v-layout>
+        <v-flex>
+          <v-sheet>
+            메타 포탈
+          </v-sheet>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 import AddListInRegist from '@/components/regist/AddListInRegist.vue';
 import RightSideCard from '@/components/regist/RightSideCard.vue';
+import RegistList from '@/components/regist/RegistList.vue';
 
 export default {
   components: {
-    AddListInRegist, RightSideCard,
+    AddListInRegist, RightSideCard, RegistList,
   },
 };
 </script>
